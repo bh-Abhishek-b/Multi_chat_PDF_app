3f021f16f6cf0d3345542865944fcfe381e982ba
Abhishek <117010831+bh-Abhishek-b@users.noreply.github.com>
2023-11-10 01:30:52 +0530

    Update multichatapp.py

--- multichatapp.py
+++ multichatapp.py
@@ -19,7 +19,7 @@ langchain.verbose = False
 
 @st.cache_resource
 def init_connection():
-    return pymongo.MongoClient(**st.secrets["mongo"])
+    return MongoClient(**st.secrets["mongo"])
 client = init_connection()
 @st.cache_data(ttl=600)
 def get_data():
